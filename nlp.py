9947a77f7dc8163b13c18bfe595a03e9f276e97c
Andrew Zirm <azirm@dark-cosmology.dk>
2014-08-12 10:14:52 -0400

    working on the tf-idf representation of the name meanings.

--- nlp.py
+++ nlp.py
@@ -1,5 +1,6 @@
 import nltk
 from nltk.tokenize import RegexpTokenizer
+from sklearn.feature_extraction.text import TfidfVectorizer
 import gensim
 import string
 import re
@@ -7,8 +8,8 @@ from db import *
 
 from nltk.corpus import stopwords
 stopw = stopwords.words('english')
-stopw.extend(['name', 'word', 'surname', 'meaning', 'history', 'born', 'diminutive', 'feminine', 'masculine'])
-stopw.extend([n['name'].lower() for n in infotab.find({}, {'name': 1})])
+stopw.extend(['name', 'word', 'surname', 'meaning', 'history', 'born', 'originally', 'means'])
+# stopw.extend([n['name'].lower() for n in infotab.find({}, {'name': 1})])
 stopw.extend(list(string.letters.lower()))
 stopw = set(stopw)
 tok1 = RegexpTokenizer("\w+")
@@ -23,11 +24,14 @@ for name in infotab.find({}):
 			# print name['name'] + '\n'
 			# print name['mean'] + '\n'
 			# print upper_re.search(name['mean']).group(1)
-			relname = infotab.find_one({'name': upper_re.search(name['mean']).group(1)})
-			name['mean'] += ' ' + relname['mean']
+			try:
+				relname = infotab.find_one({'name': upper_re.search(name['mean']).group(1)})
+				name['mean'] += ' ' + relname['mean']
+			except:
+				pass
 		wi = [w for w in tok1.tokenize(name['mean'].lower()) if w not in stopw]
 		numw = len(wi)
-		if numw < 25:
+		if numw < 15:
 			continue
 		else:
 			ddir = {'name': name['name'], '_id': name['_id']}
@@ -43,9 +47,11 @@ for name in infotab.find({}):
 	else:
 		continue
 
-f = open("/Users/azirm/Documents/insight/babynames/malletin/docs.txt", "w")
-for doc in docs:
-	f.write(string.join(doc))
-	f.write("\n")
+docsdata = [string.join(d) for d in docs]
+tfidf = TfidfVectorizer(max_features=200, use_idf=True, sublinear_tf=True, ngram_range=(1,3), max_df=0.7).fit(docsdata)
+# f = open("/Users/azirm/Documents/insight/babynames/malletin/docs.txt", "w")
+# for doc in docs:
+# 	f.write(string.join(doc))
+# 	f.write("\n")
 
-f.close()
+# f.close()
